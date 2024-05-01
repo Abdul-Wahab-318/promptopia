3591843be5e55d3a5919254a7b2e05ceb9e8341e
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2024-05-01 13:40:32 +0500

    completes search feature

--- components/Feed.js
+++ components/Feed.js
@@ -4,14 +4,33 @@ import React , { useEffect, useState } from 'react'
 import PromptCard from './PromptCard'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify'
+import useDebounce from '@app/hooks/useDebounce'
 
 const Feed = () => {
 
     const [searchText, setSearchText] = useState("")
+    const [searchTimeout , setSearchTimeout ] = useState(null)
+    const [filteredResults, setFilteredResults] = useState([])
     const [posts , setPosts] = useState([])
 
-    const handleSearchChange = (e) => {
-        setSearchText(e.target.value)
+    function filter(text) {
+        if ( text === '' )
+            return posts
+        else
+        return  posts.filter( post => post.prompt.includes(text) || post.tags.includes(text) )
+    }
+
+    const handleSearchTextChange = (e) => {
+        clearTimeout(searchTimeout);
+        setSearchText(e.target.value);
+    
+        // debounce method
+        setSearchTimeout(
+          setTimeout(() => {
+            const searchResult = filter(e.target.value);
+            setFilteredResults(searchResult);
+          }, 500)
+        );
     }
 
     useEffect(() => {
@@ -21,6 +40,7 @@ const Feed = () => {
                 const {data} = await axios.get("/api/prompt")
                 console.log(data)
                 setPosts(data)
+                setFilteredResults(data)
             }
             catch(err){
                 console.log(err)
@@ -32,16 +52,18 @@ const Feed = () => {
 
     }, [])
 
+
+
 return (
     <section className='feed mt-16'>
         <input type="text"
         value={searchText}
-        onChange={handleSearchChange}
+        onChange={handleSearchTextChange}
         required
         placeholder='Search prompt tags or username'
         className='w-full py-2 px-3 rounded shadow-md' />
         <ToastContainer/>
-        <PromptCardList data={posts} />
+        <PromptCardList data={filteredResults} />
     </section>
   )
 }
